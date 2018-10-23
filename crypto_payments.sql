3fa4e07c6fde27168e9d4434aea62a1b99d023f3
Masood <masood@999studios.com>
2018-10-23 09:40:03 +0500

    database sql file added

---
+++ crypto_payments.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2018 at 06:39 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cryptoapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crypto_payments`
+--
+
+CREATE TABLE `crypto_payments` (
+  `paymentID` int(11) UNSIGNED NOT NULL,
+  `boxID` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `boxType` enum('paymentbox','captchabox') NOT NULL,
+  `orderID` varchar(50) NOT NULL DEFAULT '',
+  `userID` varchar(50) NOT NULL DEFAULT '',
+  `countryID` varchar(3) NOT NULL DEFAULT '',
+  `coinLabel` varchar(6) NOT NULL DEFAULT '',
+  `amount` double(20,8) NOT NULL DEFAULT '0.00000000',
+  `amountUSD` double(20,8) NOT NULL DEFAULT '0.00000000',
+  `unrecognised` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `addr` varchar(34) NOT NULL DEFAULT '',
+  `txID` char(64) NOT NULL DEFAULT '',
+  `txDate` datetime DEFAULT NULL,
+  `txConfirmed` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `txCheckDate` datetime DEFAULT NULL,
+  `processed` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `processedDate` datetime DEFAULT NULL,
+  `recordCreated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `crypto_payments`
+--
+ALTER TABLE `crypto_payments`
+  ADD PRIMARY KEY (`paymentID`),
+  ADD UNIQUE KEY `key3` (`boxID`,`orderID`,`userID`,`txID`,`amount`,`addr`),
+  ADD KEY `boxID` (`boxID`),
+  ADD KEY `boxType` (`boxType`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `countryID` (`countryID`),
+  ADD KEY `orderID` (`orderID`),
+  ADD KEY `amount` (`amount`),
+  ADD KEY `amountUSD` (`amountUSD`),
+  ADD KEY `coinLabel` (`coinLabel`),
+  ADD KEY `unrecognised` (`unrecognised`),
+  ADD KEY `addr` (`addr`),
+  ADD KEY `txID` (`txID`),
+  ADD KEY `txDate` (`txDate`),
+  ADD KEY `txConfirmed` (`txConfirmed`),
+  ADD KEY `txCheckDate` (`txCheckDate`),
+  ADD KEY `processed` (`processed`),
+  ADD KEY `processedDate` (`processedDate`),
+  ADD KEY `recordCreated` (`recordCreated`),
+  ADD KEY `key1` (`boxID`,`orderID`),
+  ADD KEY `key2` (`boxID`,`orderID`,`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `crypto_payments`
+--
+ALTER TABLE `crypto_payments`
+  MODIFY `paymentID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
